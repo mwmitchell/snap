6e8b86bceecbeac06db7203b0fa764550f1497e9
Matt Mitchell <mattmitchell@matt-mitchells-macbook-pro-15.local>
2008-06-27 18:31:19 -0400

    Mergin' totally new files with next commit

--- github-env-test.rb
+++
@@ -1,5 +0,0 @@
-require 'rubygems/specification'
-data = File.read('snap.gemspec')
-spec = nil
-Thread.new { spec = eval("$SAFE = 3\n#{data}") }.join
-puts spec
--- spec/common.rb
+++
@@ -1,7 +0,0 @@
-$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'snap'
-require 'spec'
-
-def new_rack_env(path='/', attrs={})
-  Rack::MockRequest.env_for(path, attrs)
-end
