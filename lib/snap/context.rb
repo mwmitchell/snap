24a1b98fcf9c6d353c8a8d374a9fc42b7308d9a5
Matt Mitchell <mattmitchell@matt-mitchells-macbook-pro-15.local>
2008-08-05 23:20:15 -0400

    'replacing with completely new core'

--- lib/snap/context.rb
+++
@@ -1,9 +0,0 @@
-#
-#
-#
-module Snap::Context
-  autoload :Base, 'snap/context/base'
-  autoload :Events, 'snap/context/events'
-  autoload :Matcher, 'snap/context/matcher'
-  autoload :Actions, 'snap/context/actions'
-end
