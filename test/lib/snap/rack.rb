e806850c42d8ffdcce18a91138f0e06616af0837
Matt Mitchell <goodieboy@gmail.com>
2009-01-02 15:07:01 -0500

    Removed test/lib and test/README.rdoc

--- test/lib/snap/rack.rb
+++
@@ -1,7 +0,0 @@
-module Snap::Rack
-  
-  autoload :Request, 'snap/rack/request'
-  autoload :Response, 'snap/rack/response'
-  autoload :App, 'snap/rack/app'
-  
-end
--- test/lib/snap/rack/response.rb
+++
@@ -1,9 +0,0 @@
-class Snap::Rack::Response < Rack::Response
-  
-  def headers=(headers={})
-    headers.each do |k,v|
-      self[k]=v
-    end
-  end
-  
-end
